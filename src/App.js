7123c38dcebbdd2ae256bec06f8640e4db1d8c5e
Frederic Mendes <fredmanumendes@gmail.com>
2018-12-31 16:31:38 +0100

    intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+<h1>A great Start</h1>
       </div>
     );
   }
