8b925247ec18d1b3d3ddc393563a912afddd85f9
FredMds <fredmanumendes@gmail.com>
2019-03-26 00:01:56 +0000

    less and more button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-<h1>A great Start</h1>
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
     );
   }
